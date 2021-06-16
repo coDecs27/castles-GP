880b0cd8b7d5a1dd76edeec7f1a84b467613e970
Matthias Kutz <matthias.kutz@gmx.net>
2021-06-16 17:39:50 +0200

    added routes, controller and all pages

---
+++ app/controllers/castles_controller.rb
@@ -0,0 +1,16 @@
+class CastlesController < ApplicationController
+  def index
+  end
+
+  def hohenrechberg
+  end
+
+  def hohenstaufen
+  end
+
+  def rosenstein
+  end
+
+  def reussenstein
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'castles#index'
+  get 'hohenrechberg', to: 'castles#hohenrechberg'
+  get 'hohenstaufen', to: 'castles#hohenstaufen'
+  get 'rosenstein', to: 'castles#rosenstein'
+  get 'reussenstein', to: 'castles#reussenstein'
 end
